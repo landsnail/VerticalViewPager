74d067aae3df9e9ea8616eb9c8cdf0bcde8d778c
landsnail <landsnail@qq.com>
2014-11-24 10:18:34 +0800

    修复减少依赖高级环境包，web加载不成功的问题

--- simple/src/com/example/verticalviewpagersample/MainActivity.java
+++ simple/src/com/example/verticalviewpagersample/MainActivity.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import com.view.vertical.VerticalViewPager;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,58 +15,62 @@ import android.webkit.WebView;
  * 
  * 
  * @author landsnail---hongyun.fang
- * @email  fanghongyun@gmail.com
- *
+ * @email fanghongyun@gmail.com
+ * 
  */
 
-public class MainActivity extends ActionBarActivity {
-	
-	VerticalViewPager  verticalViewPager;
-	List<View>  viewPageList;
- 	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.detail);
-        ///
-        verticalViewPager = (VerticalViewPager) findViewById(R.id.verticalViewPager);
+public class MainActivity extends Activity {
 
-        viewPageList = new ArrayList<View>();
-        View cell_top = getLayoutInflater().inflate(R.layout.cell_top, null);
-        viewPageList.add(cell_top);
+	VerticalViewPager verticalViewPager;
+	List<View> viewPageList;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.detail);
+		// /
+		verticalViewPager = (VerticalViewPager) findViewById(R.id.verticalViewPager);
 
-        View cell_bottom =getLayoutInflater().inflate(R.layout.cell_bottom, null);
-        viewPageList.add(cell_bottom);
-        
-       WebView web= (WebView) cell_bottom.findViewById(R.id.web_content);
-       web.loadUrl("www.baidu.com");
+		viewPageList = new ArrayList<View>();
+		View cell_top = getLayoutInflater().inflate(R.layout.cell_top, null);
+		viewPageList.add(cell_top);
 
+		View cell_bottom = getLayoutInflater().inflate(R.layout.cell_bottom,
+				null);
+		viewPageList.add(cell_bottom);
 
-        ////初始化控件数据
-        verticalViewPager.setAdapter(new com.view.vertical.PagerAdapter() {
-            @Override
-            public int getCount() {
-                return viewPageList.size();
-            }
+		WebView web = (WebView) cell_bottom.findViewById(R.id.web_content);
+		//设置WebView属性，能够执行Javascript脚本 
+		web.getSettings().setJavaScriptEnabled(true); 
+		web.loadUrl("http://www.baidu.com");
 
-            @Override
-            public void destroyItem(ViewGroup container, int position, Object object) {
-                super.destroyItem(container, position, object);
-                container.removeView(viewPageList.get(position % viewPageList.size()));
-            }
+		// //初始化控件数据
+		verticalViewPager.setAdapter(new com.view.vertical.PagerAdapter() {
+			@Override
+			public int getCount() {
+				return viewPageList.size();
+			}
 
-            @Override
-            public Object instantiateItem(ViewGroup container, int position) {
-                container.addView(viewPageList.get(position % viewPageList.size()));
-                return viewPageList.get(position);
-            }
+			@Override
+			public void destroyItem(ViewGroup container, int position,
+					Object object) {
+				super.destroyItem(container, position, object);
+				container.removeView(viewPageList.get(position
+						% viewPageList.size()));
+			}
 
-            @Override
-            public boolean isViewFromObject(View view, Object object) {
-                return view == object;
-            }
-        });
-        verticalViewPager.setCurrentItem(0);
-    }
+			@Override
+			public Object instantiateItem(ViewGroup container, int position) {
+				container.addView(viewPageList.get(position
+						% viewPageList.size()));
+				return viewPageList.get(position);
+			}
+
+			@Override
+			public boolean isViewFromObject(View view, Object object) {
+				return view == object;
+			}
+		});
+		verticalViewPager.setCurrentItem(0);
+	}
 }
